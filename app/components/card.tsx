d4a5c362adad3a5b4bd3a36d2abe7df146eb7cdd
Noam Suissa <noam.suissa01@gmail.com>
2024-05-10 14:04:39 -0400

    fit contact page to theme

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -5,12 +5,13 @@ import {
 	useMotionValue,
 	useSpring,
 } from "framer-motion";
-
-import { MouseEventHandler, PropsWithChildren } from "react";
+import Particles from "../components/particles";
+import { MouseEventHandler, PropsWithChildren, useState } from "react";
 
 export const Card: React.FC<PropsWithChildren> = ({ children }) => {
 	const mouseX = useSpring(0, { stiffness: 500, damping: 100 });
 	const mouseY = useSpring(0, { stiffness: 500, damping: 100 });
+	const [isHovered, setIsHovered] = useState(false);
 
 	function onMouseMove({ currentTarget, clientX, clientY }: any) {
 		const { left, top } = currentTarget.getBoundingClientRect();
@@ -23,7 +24,9 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<div
 			onMouseMove={onMouseMove}
-			className="overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600 "
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+			className="overflow-hidden relative duration-700 border rounded-xl hover:bg-slate-950 group md:gap-8 hover:border-zinc-400/50 border-zinc-600 "
 		>
 			<div className="pointer-events-none">
 				<div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
@@ -31,6 +34,16 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
 					className="absolute inset-0 z-10  bg-gradient-to-br opacity-100  via-zinc-100/10  transition duration-1000 group-hover:opacity-50 "
 					style={style}
 				/>
+				{isHovered && (
+					<Particles
+						className="absolute inset-0 -z-11"
+						quantity={100}
+						staticity={15}
+						r={0}
+						g={255}
+						b={0}
+					/>
+				)}
 				<motion.div
 					className="absolute inset-0 z-10 opacity-0 mix-blend-overlay transition duration-1000 group-hover:opacity-100"
 					style={style}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,33 +1,33 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Twitter, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/noam-suissa-m-sc-b-eng-ba937b172/recent-activity/all/",
+		label: "LinkedIn",
+		handle: "Noam Suissa",
 	},
 	{
 		icon: <Mail size={20} />,
 		href: "mailto:dev@chronark.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "noam.suissa01@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/noamsuissa",
 		label: "Github",
-		handle: "chronark",
+		handle: "noamsuissa",
 	},
 ];
 
 export default function Example() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+		<div className=" bg-gradient-to-tl from-white via-slate-950/20 to-white">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
 				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
@@ -39,17 +39,17 @@ export default function Example() {
 								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
 							>
 								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+									className="absolute w-px h-2/3 bg-gradient-to-b from-slate-600 via-slate-500/50 to-transparent"
 									aria-hidden="true"
 								/>
 								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
 									{s.icon}
 								</span>{" "}
 								<div className="z-10 flex flex-col items-center">
-									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-slate-950 group-hover:text-slate-50 font-display">
 										{s.handle}
 									</span>
-									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+									<span className="mt-4 text-sm text-center duration-150 text-slate-9500 group-hover:text-slate-50">
 										{s.label}
 									</span>
 								</div>
