b79ebf1c98d77cd69a69446daad0cb1eed307e00
Noam Suissa <noam.suissa01@gmail.com>
2024-09-26 20:35:00 -0400

    added link to blog

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Particles from "./components/particles";
 const navigation = [
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
+  { name: "Blog", href: "https://blog.noamaidev.com", target: "_blank", rel: "noopener noreferrer" },
 ];
 
 export default function Home() {
